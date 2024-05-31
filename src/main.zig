bd02ee72036f8e62d52503bb20646298d91fd865
DrunkTaric <comborush.init@proton.me>
2024-05-31 08:58:11 +0300

    adding the arg iteration

--- src/main.zig
+++ src/main.zig
@@ -35,9 +35,12 @@ const CommandTypes = union(enum) {
     }
 };
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    var argsIterator = try std.process.ArgIterator.initWithAllocator(allocator);
+    defer argsIterator.deinit();
+    // Skipping the first arg
+    _ = argsIterator.next();
+
 }
