2e8a004e7fa8b8718874dfd7e6c6504fe8fc3b03
DrunkTaric <comborush.init@proton.me>
2024-06-01 22:30:02 +0300

    cat file TMR

--- src/commands/cat-file.zig
+++ src/commands/cat-file.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const fs = std.fs.cwd();
+const zlib = std.compress.zlib;
 const Option = @import("../utils/option.zig").Option;
 
 pub fn init(argIter: std.process.ArgIterator, alloc: std.mem.Allocator) void {
@@ -7,7 +9,17 @@ pub fn init(argIter: std.process.ArgIterator, alloc: std.mem.Allocator) void {
     defer options.free();
 
     options.parse();
-    options.print();
-    std.debug.print("the value is: {?}\n", .{options.get("-p")});
-    std.debug.print("the value is: {?}\n", .{options.get("-su")});
+
+    if (options.get("-p")) |entry| {
+        const dirname = entry.V.?[0..2];
+        const filename = entry.V.?[2..];
+        const path = ".lit/objects/" ++ dirname ++ "/" ++ filename;
+        var file = try fs.openFile(path, .{ .iterate = true });
+        if (file) {
+            file.readAll();
+            std.debug.print("Lebron is real with value: {any}\n", .{dirname});
+        } else {
+            std.log.err("Error happend while trying to access the file", .{});
+        }
+    }
 }
