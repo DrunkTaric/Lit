a22f73d191eb964e0c74a3c8ea2d6f2a52355287
DrunkTaric <comborush.init@proton.me>
2024-06-02 19:52:16 +0300

    WIP

--- src/commands/cat-file.zig
+++ src/commands/cat-file.zig
@@ -3,7 +3,7 @@ const fs = std.fs.cwd();
 const zlib = std.compress.zlib;
 const Option = @import("../utils/option.zig").Option;
 
-pub fn init(argIter: std.process.ArgIterator, alloc: std.mem.Allocator) void {
+pub fn init(argIter: std.process.ArgIterator, alloc: std.mem.Allocator) !void {
     var arena = std.heap.ArenaAllocator.init(alloc);
     var options = Option.init(arena.allocator(), argIter);
     defer options.free();
@@ -12,14 +12,32 @@ pub fn init(argIter: std.process.ArgIterator, alloc: std.mem.Allocator) void {
 
     if (options.get("-p")) |entry| {
         const dirname = entry.V.?[0..2];
-        const filename = entry.V.?[2..];
-        const path = ".lit/objects/" ++ dirname ++ "/" ++ filename;
-        var file = try fs.openFile(path, .{ .iterate = true });
-        if (file) {
-            file.readAll();
-            std.debug.print("Lebron is real with value: {any}\n", .{dirname});
-        } else {
-            std.log.err("Error happend while trying to access the file", .{});
+        const path = ".lit/objects/" ++ dirname;
+        var dir = try fs.openDir(path, .{ .iterate = true });
+        var iterator = dir.iterate();
+        while (try iterator.next()) |file| {
+            if (file.kind == .file) {
+                if (std.mem.eql(u8, file.name, entry.V.?[2..])) {
+                    const target = try fs.openFile(path ++ file.name, .{ .mode = .read_only });
+                    defer target.close();
+
+                    var buffered = std.io.bufferedReader(target.reader());
+                    var reader = buffered.reader();
+
+                    var arr = std.ArrayList(u8).init(alloc);
+                    defer arr.deinit();
+
+                    while (true) {
+                        reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch (err) {
+                            error.EndOfStream => break,
+                            else => return err,
+                        };
+                        arr.clearRetainingCapacity();
+                    }
+
+                    std.debug.print("things: {?}", .{reader});
+                }
+            }
         }
     }
 }
