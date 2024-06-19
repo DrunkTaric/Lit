1d0d54556e4e3a12fc325f09f4126601e70d4815
comborush <comborush.init@proton.me>
2024-06-20 01:18:25 +0300

    done with this command

--- src/commands/cat-file.zig
+++ src/commands/cat-file.zig
@@ -15,27 +15,38 @@ pub fn init(argIter: std.process.ArgIterator, alloc: std.mem.Allocator) !void {
     if (options.get("-p")) |entry| {
         if (entry.V) |value| {
             var path = String.init(arena.allocator());
+            defer path.deinit();
+
             try path.concat(".lit/objects/" ++ value[0..2]);
             var dir = try fs.openDir(path.str(), .{ .iterate = true });
             var iterator = dir.iterate();
             while (try iterator.next()) |file| {
                 if (file.kind == .file) {
-                    if (std.mem.eql(u8, file.name, value[2..])) {
+                    var filename = String.init(arena.allocator());
+                    defer filename.deinit();
+
+                    try filename.concat(value[2..]);
+
+                    if (std.mem.eql(u8, file.name, filename.str())) {
                         try path.concat("/");
-                        try path.concat(value[2..]);
+                        try path.concat(filename.str());
+
                         const target = try fs.openFile(path.str(), .{ .mode = .read_only });
                         defer target.close();
 
-                        var buffered = std.io.bufferedReader(target.reader());
-                        var reader = buffered.reader();
+                        var input_buffer = std.io.bufferedReader(target.reader());
+                        const in_reader = input_buffer.reader();
 
-                        var arr = std.ArrayList(u8).init(alloc);
-                        defer arr.deinit();
+                        var decompressed_data: [4096]u8 = undefined;
+                        var output_buffer = std.io.fixedBufferStream(&decompressed_data);
+                        const output_stream = output_buffer.writer();
 
-                        var buf: [1024]u8 = undefined;
+                        const result = std.compress.zlib.decompress(in_reader, output_stream);
 
-                        while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-                            std.debug.print("{s}\n", .{line});
+                        if (result) |_| {
+                            std.log.debug("Decompressed data: {s}\n", .{decompressed_data[0..]});
+                        } else |err| {
+                            std.debug.print("Decompression failed: {}\n", .{err});
                         }
                     }
                 }
