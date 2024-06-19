72907f09db7512b314bc7f6365e06dec0c5fc42f
comborush <comborush.init@proton.me>
2024-06-19 23:50:17 +0300

    removing uselss stuff

--- src/commands/cat-file.zig
+++ src/commands/cat-file.zig
@@ -12,31 +12,10 @@ pub fn init(argIter: std.process.ArgIterator, alloc: std.mem.Allocator) !void {
     options.parse();
 
     if (options.get("-p")) |entry| {
-        const dirname = entry.V.?[0..2];
-        const path = ".lit/objects/" ++ dirname;
-        var dir = try fs.openDir(path, .{ .iterate = true });
-        var iterator = dir.iterate();
-        while (try iterator.next()) |file| {
-            if (file.kind == .file) {
-                if (std.mem.eql(u8, file.name, entry.V.?[2..])) {
-                    const target = try fs.openFile(path ++ file.name, .{ .mode = .read_only });
-                    defer target.close();
 
-                    var buffered = std.io.bufferedReader(target.reader());
-                    var reader = buffered.reader();
 
-                    var arr = std.ArrayList(u8).init(alloc);
-                    defer arr.deinit();
 
-                    while (true) {
-                        reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch (err) {
-                            error.EndOfStream => break,
-                            else => return err,
-                        };
-                        arr.clearRetainingCapacity();
-                    }
 
-                    std.debug.print("things: {?}", .{reader});
                 }
             }
         }
