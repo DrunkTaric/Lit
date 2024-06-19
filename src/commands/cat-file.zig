d691db8137c4c2b4e30f066478181d82befe1849
comborush <comborush.init@proton.me>
2024-06-19 23:49:46 +0300

    oops memory leak

--- src/commands/cat-file.zig
+++ src/commands/cat-file.zig
@@ -5,6 +5,7 @@ const Option = @import("../utils/option.zig").Option;
 
 pub fn init(argIter: std.process.ArgIterator, alloc: std.mem.Allocator) !void {
     var arena = std.heap.ArenaAllocator.init(alloc);
+    defer arena.deinit();
     var options = Option.init(arena.allocator(), argIter);
     defer options.free();
 
